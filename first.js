ae23c1d0b104afc8b9229336084b10b2ae493f8b
Graceomole <105713462+Graceomole@users.noreply.github.com>
2022-06-20 16:33:56 +0100

    Add files via upload

---
+++ first.js
@@ -0,0 +1,11 @@
+let firstName = prompt("What  is your name? ")
+let lastName = " Omoleye"
+let age  = prompt("What is your age? ")
+alert("Hello my name is " + firstName + lastName  + " i am " + age + "years"  )
+/*
+kkkkkkkk
+llljj
+kkkkk...k
+ll
+/*
+alert('Coming to you from first.js')
